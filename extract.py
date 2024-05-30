f7cd61617488693340a132c08c83db21f8550b91
acooney1 <acooney@bowdoin.edu>
2024-05-30 13:38:25 -0400

    removed duplicates

--- extract.py
+++ extract.py
@@ -1,2 +1,2 @@
-#extract unique words from large txt file
-#move words into dictionary (key = word, value = array of embeddings) iteratively, if a duplicate -- then do not add
+# extract unique words from large txt file
+# move words into dictionary (key = word, value = array of embeddings) iteratively, if a duplicate -- then do not add
