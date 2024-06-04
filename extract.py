08f54c02f35ddd530241ac1fbff050741fabe3f4
acooney1 <acooney@bowdoin.edu>
2024-06-04 10:18:06 -0400

    running into issues converting string values to floats

--- extract.py
+++ extract.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 def extract_unique(file):
     file = open("forager/data/fluency_lists/speech2vec_100.txt", "r")
-    unique_words = {}
+    #unique_words = {}
     word_list = []
     for line in file.readlines():
         split_line = line.split(" ")
-        unique_words[split_line[0]] = split_line[1: ]
+        #unique_words[split_line[0]] = split_line[1: ]
         word_list.append(split_line[0])
     
     word_list_df = pd.DataFrame(word_list)
--- vocab_check.py
+++ vocab_check.py
@@ -3,17 +3,30 @@ import difflib
 import numpy as np
 
 
-def vocab_check(vocab_file, data_file):
-    speech2vec_vocab_df = pd.read_csv("forager/data/lexical_data/speech2vec_vocab.csv")
-    food_data_df = pd.read_csv("forager/data/fluency_lists/food_data - Sheet1.csv")
-    for index,row in speech2vec_vocab_df.iterrows():
-
+# def vocab_check(vocab_file, data_file):
+#     speech2vec_vocab_df = pd.read_csv("forager/data/lexical_data/speech2vec_vocab.csv")
+#     food_data_df = pd.read_csv("forager/data/fluency_lists/food_data - Sheet1.csv")
+#     for index,row in speech2vec_vocab_df.iterrows():
 
 def cosine_similarity(word1, word2):
-    vector1 = 
-    vector2 = 
+    file = open("forager/data/fluency_lists/speech2vec_100.txt", "r")
+    word_embeddings = {}
+    new_file = file.readlines()
+    first_line_skipped = new_file[1: ]
+    
+    for line in first_line_skipped:
+        split_line = line.split(" ")
+        key = split_line[0]
+        string_list = split_line[1: ]
+        float_list = list(map(float, string_list))
+        word_embeddings[key] = float_list
+        
+    vector1 = word_embeddings[word1]
+    vector2 = word_embeddings[word2]
     dot_product = np.dot(vector1, vector2)
     magnitude1 = np.linalg.norm(vector1)
     magnitude2 = np.linalg.norm(vector2)
     cosine_sim = dot_product/(magnitude1*magnitude2)
     print("Cosine Similarity:", cosine_sim)
+
+cosine_similarity("apple", "apricot")
