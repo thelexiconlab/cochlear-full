af2ba2eaed2cfe8f5a537e5bbe9902d3d8546674
acooney1 <acooney@bowdoin.edu>
2024-06-05 09:38:57 -0400

    working on comparing food data and speech2vec_vocab

--- vocab_check.py
+++ vocab_check.py
@@ -2,12 +2,6 @@ import pandas as pd
 import difflib
 import numpy as np
 
-
-# def vocab_check(vocab_file, data_file):
-#     speech2vec_vocab_df = pd.read_csv("forager/data/lexical_data/speech2vec_vocab.csv")
-#     food_data_df = pd.read_csv("forager/data/fluency_lists/food_data - Sheet1.csv")
-#     for index,row in speech2vec_vocab_df.iterrows():
-
 def cosine_similarity(word1, word2):
     file = open("forager/data/fluency_lists/speech2vec_100.txt", "r")
     word_embeddings = {}
@@ -37,4 +31,42 @@ def cosine_similarity(word1, word2):
     cosine_sim = dot_product/(magnitude1*magnitude2)
     print("Cosine Similarity:", cosine_sim)
 
-cosine_similarity("apple", "the")
+#cosine_similarity("apple", "peach")
+
+def vocab_check(vocab_file, data_file):
+    speech2vec_vocab_df = pd.read_csv("forager/data/lexical_data/speech2vec_vocab.csv")
+    food_data_df = pd.read_csv("forager/data/fluency_lists/food_data - Sheet1.csv")
+    
+    vocab = open("forager/data/fluency_lists/speech2vec_100.txt", "r")
+    speech2vec_vocab_list = []
+    for line in vocab.readlines():
+        split_line = line.split(" ")
+        speech2vec_vocab_list.append(split_line[0])
+    
+
+    data = open("forager/data/fluency_lists/food_data.txt", "r")
+    food_data_list = []
+    for line in data.readlines():
+        split_line = line.split(" ")
+        food_data_list.append(split_line[0])
+    
+    presence_list = []
+    replacements = []
+    for item in food_data_list:
+        if item in speech2vec_vocab_list:
+            presence_list.append("FOUND")
+            replacements.append(item)
+            # add column evaluation and write FOUND
+        else:
+            presence_list.append("NOT FOUND")
+            replacements.append(difflib.get_close_matches(item, speech2vec_vocab_list))
+            #add column evaluation and write NOT FOUND
+
+    #incorporate difflib to evalaute potential replacements with get_close_matches
+    food_data_df["evaluation"] = presence_list
+    food_data_df["difflib"] = replacements
+    
+    final_csv_path = "forager/output/cochlear_food_fulldata_forager_results/speech2vec_vocab_check.csv"
+    food_data_df.to_csv(final_csv_path, index=False)
+
+vocab_check("forager/data/fluency_lists/speech2vec_100.txt", "forager/data/fluency_lists/food_data.txt")
