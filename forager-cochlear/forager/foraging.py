b7b7536acc62383bd53096855badf0770ed20fa7
channinghambric <c.hambric@bowdoin.edu>
2025-03-11 14:55:07 -0400

    sep beta models

--- forager-cochlear/forager/foraging.py
+++ forager-cochlear/forager/foraging.py
@@ -94,7 +94,7 @@ class forage:
             elif switchvals[k]==1: ## "dip" based on sim-drop
                 # If similarity dips, P of item is based on frequency 
                 numrat = pow(freql[k],beta[0]) 
-                denrat = sum(pow(freqh[k],beta[0]))
+                denrat = sum(pow(freqh[k],beta[0]))   
 
             else:    
                 # if not first item then its probability is based on its similarity to prev item AND frequency
